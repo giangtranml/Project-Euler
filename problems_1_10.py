6324e3db24912139fdb9e2453114c3cd6681563b
tgiang <giangtran240896@gmail.com>
2018-10-31 21:32:57 +0700

    update

--- problems_1_10.py
+++ problems_1_10.py
@@ -109,11 +109,11 @@ def problem_4():
     """
     def reversed_num(num):
         r_num = 0
-        while num > 0:
+        while num//10 > 0:
             r_num += num % 10
             r_num *= 10
             num //= 10
-        return r_num//10
+        return r_num
 
     def is_palindrome(num):
         reversed = reversed_num(num)
@@ -132,3 +132,94 @@ def problem_4():
 
     res = max(l_)
     print(res)
+
+def problem_5():
+    """
+        Name: Smallest multiple.
+
+        2520 is the smallest number that can be divided by each of
+            the numbers from 1 to 10 without any remainder.
+
+        What is the smallest positive number that is evenly
+            divisible by all of the numbers from 1 to 20?
+
+        --------------------------------------------------------------------
+
+        Find LCM(least common multiple) in a range 1 to 20.
+
+        Prime number: 1, 2, 3, 5, 7, 11, 13, 17, 19.
+
+            4         6         8          9      10      12        14      15      16           18         20
+           / \       / \       / \        / \     / \     / \       / \     / \     / \         / \         / \
+          2   2     2   3     2   4      3   3   2   5   2   6     2   7   3   5   2  8        2   9       2  10
+                                 / \                        / \                      / \          / \         / \
+                                2   2                      2   3                    2   4        3   3       2   5
+                                                                                       / \
+                                                                                      2   2
+          28              35                 12             32
+        2   14          5    7             2    6         2    16
+           2  7                               2   3          2    8
+                                                                2   4
+                                                                   2  2
+
+            | Prime factor
+        ----------------------------------------------------------------
+         01 | 1
+         02 |   2
+         03 |               3
+         04 |   2  2
+         05 |                       5
+         06 |   2           3
+         07 |                           7
+         08 |   2  2  2
+         09 |               3   3
+         10 |   2                   5
+         11 |                                   11
+         12 |   2  2        3
+         13 |                                           13
+         14 |   2                       7
+         15 |               3       5
+         16 |   2  2  2  2
+         17 |                                               17
+         18 |   2           3   3
+         19 |                                                   19
+         20 |   2  2                5
+
+         LCM(1, 20) = 2^4 * 3^2 * 5 * 7 * 11 * 13 * 17 * 19
+
+         That is, get the longest sequence length of a factor prime.
+    """
+    n = 1
+    for i in range(2, 21):
+        n *= i
+
+    print(n)
+
+def problem_6():
+    """
+        Name: Sum square difference.
+
+        The sum of the squares of the first ten natural numbers is,
+            1^2 + 2^2 + ... + 10^2 = 385
+        The square of the sum of the first ten natural numbers is,
+            (1 + 2 + ... + 10)^2 = 55^2 = 3025
+        Hence the difference between the sum of the squares of the
+            first ten natural numbers and the square of
+            the sum is 3025 − 385 = 2640.
+
+        Find the difference between the sum of the squares of
+            the first one hundred natural numbers and the square of the sum.
+    """
+
+    sum_of_square = 0
+    square_of_sum = 0
+    for i in range(1, 101):
+        square_of_sum += i
+        sum_of_square += i**2
+    square_of_sum **= 2
+    print(sum_of_square)
+    print(square_of_sum)
+    diff = square_of_sum - sum_of_square
+    print(diff)
+
+problem_6()
