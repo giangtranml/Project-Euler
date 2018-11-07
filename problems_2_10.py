ca469fffc98b57e7caf2cce3d3d764cc0cef427c
Guest <giangtran240896@gmail.com>
2018-11-07 22:17:22 +0700

    update

--- problems_2_10.py
+++ problems_2_10.py
@@ -26,7 +26,7 @@ def problem_11():
     grid = grid.strip()
     grid = grid.split("\n")
     for i, g in enumerate(grid):
-        grid[i] = list(map(int, g.strip().split(" ")))
+        grid[i] = list(path(int, g.strip().split(" ")))
     m = len(grid)
     n = len(grid[0])
     res = 0
@@ -106,4 +106,51 @@ def problem_14():
             res = i
     print(res)
 
-problem_14()
+def problem_15():
+    """
+        Starting in the top left corner of a 2×2 grid,
+        and only being able to move to the right and down,
+        there are exactly 6 routes to the bottom right corner.
+
+        How many such routes are there through a 20×20 grid?
+
+        -------------------------------------------------------------------
+
+        Dynamic Programming Approach:
+
+        -------------------------
+        | 2 | 3 | 4 | 5 | 6 | 7 |
+        -------------------------
+        | 3 | 6 | 10| 15| 21| 28|
+        -------------------------
+        | 4 | 10| 20| 35|   |   |
+        -------------------------
+        | 5 | 15| 35| 70|   |   |
+        -------------------------
+        | 6 |   |   |   |   |   |
+        -------------------------
+        | 7 |   |   |   |   |   |
+        -------------------------
+
+        --------------------------------------------------------------------
+        2x2: (2 + 1)*2
+        3x3: (2 + 2)
+    """
+
+    n = 20
+    path = [[1]*n for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            if i == 0 and j > 0:
+                path[i][j] = path[i][j-1] + 1
+                continue
+            if j == 0 and i > 0:
+                path[i][j] = path[i-1][j] + 1
+                continue
+            path[i][j] = path[i-1][j] + path[i][j-1]
+
+    for p in path:
+        print(p)
+    print(path[n-1][n-1])
+
+problem_15()
